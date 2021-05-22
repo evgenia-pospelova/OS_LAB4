06f3378a627357adab3edf4256a16263d452a4a9
evgenia-pospelova <72083483+evgenia-pospelova@users.noreply.github.com>
2021-05-22 17:52:28 +0300

    Add files via upload

---
+++ OS_LAB4_1.cpp
@@ -0,0 +1,22 @@
+// OS_LAB4_1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <windows.h>
+#include <stdlib.h>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    cout << "There are " << argc << " arguments:\n";
+
+    for (int count = 0; count < argc; count++){
+        cout << count << ". " << argv[count] << '\n';
+        cout.flush();
+        Sleep(1);
+    }
+
+    cout << endl << "Labs id: " << GetCurrentProcessId << endl;
+    cout << "Labs parent id: " << GetProcessId << endl;
+    exit(31);
+}
